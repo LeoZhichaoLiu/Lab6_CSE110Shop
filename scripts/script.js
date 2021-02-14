e8f946aeda33ce627ff9cb5c88ba43b17ff9e9b6
LeoZhichaoLiu <49352811+LeoZhichaoLiu@users.noreply.github.com>
2021-02-14 14:09:38 +0800

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,24 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  //localStorage.clear();
+  if (localStorage.getItem("count") != null) {
+     var num = document.getElementById("cart-count");
+     num.textContent = localStorage.getItem("count");
+  } 
+
+  fetch("https://fakestoreapi.com/products")
+    .then(response => response.json())
+    .then(data => {
+
+      localStorage.setItem("data", data);
+      var list = document.getElementById("product-list");
+
+      for (var i = 0; i < data.length; i++) {
+          var item = document.createElement("product-item");
+          item.setAttribute('data', JSON.stringify(data[i]));
+          list.appendChild(item);
+      }
+    })
+
 });
